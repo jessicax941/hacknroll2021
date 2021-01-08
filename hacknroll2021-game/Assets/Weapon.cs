20b08c1cdbeb8fe37e0ce42b2873f5d4b9b5c4fd
Jessica Phua Shu Xin <jessicax941@gmail.com>
2021-01-08 20:51:22 +0800

    Add bullet prefab

---
+++ hacknroll2021-game/Assets/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+
+    }
+}

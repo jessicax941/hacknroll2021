86684259997a327f2efd460d976fdde09a250f9b
MC <dandford@gmail.com>
2021-01-09 11:15:14 +0800

    Enemy

---
+++ hacknroll2021-game/Assets/EnemyBehavior.cs
@@ -0,0 +1,41 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private float startingX = 23.2f;
+    private float startingY = 13.2f;
+    private float horizontalMove = 0f;
+    private float verticalMove = 0f;
+    private Rigidbody2D rb;
+    public Transform player;
+    private float startTimeBtwShots;
+    public float timeBtwShots;
+
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.position = new Vector2(startingX, startingY); // set starting position
+        rb.rotation = -90f;
+        startTimeBtwShots = 2f;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.position = new Vector2(startingX, player.position.y);
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        } else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ hacknroll2021-game/Assets/EnemyShoot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+        this.speed = 20f;
+        rb = GetComponent<Rigidbody2D>();
+        rb.rotation = -90f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveToA = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
